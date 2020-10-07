2299c3185eff3eda9c52cb5af2cbf966002a8a58
Tehjean Powell <tehjean.powell@gmail.com>
2020-10-07 18:28:48 -0500

    added updated version of my resume

--- src/home.js
+++ src/home.js
@@ -23,7 +23,7 @@ function Home() {
                     &nbsp;<big className="middot"><strong>&middot;</strong></big>&nbsp;
                     <li><a href="#Portfolio" data-offset="100">Portfolio</a></li>
                     &nbsp;<big className="middot"><strong>&middot;</strong></big>&nbsp;
-                    <li><a href={process.env.PUBLIC_URL + "/_TehjeanPowell_2020_Resume3.pdf"}>Resumé</a></li>
+                    <li><a href={process.env.PUBLIC_URL + "/_TehjeanPowell_2020_Resume4.pdf"}>Resumé</a></li>
                 </ul>
             </nav>
         </section>
